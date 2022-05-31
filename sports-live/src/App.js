f6552e92ec946443afc29923c59f8e2ea6c84006
AtharvaCM <necrons999@gmail.com>
2022-05-31 21:51:24 +0530

    redirect to /Home

--- sports-live/src/App.js
+++ sports-live/src/App.js
@@ -3,11 +3,20 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
-import { Outlet } from "react-router-dom";
+import { useEffect } from "react";
+
+import { Outlet, useNavigate } from "react-router-dom";
 
 import Navbar from "./Components/Navbar";
 
 function App() {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // When the page loads redirect to /Home
+    navigate("/Home");
+  }, [navigate]);
+
   return (
     <>
       <Navbar></Navbar>
